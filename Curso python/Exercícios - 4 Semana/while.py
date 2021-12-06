87547e9ad01803e37989b9c1fe7987a350cc26b6
Lucas Farias De Carvalho Rocha <61882924+LucasRocha42@users.noreply.github.com>
2021-12-06 11:33:08 +0000

    Comentando While

--- Curso python/Exercícios - 4 Semana/while.py
+++ Curso python/Exercícios - 4 Semana/while.py
@@ -1,9 +1,8 @@
-print("Digite os numeros")
-soma = 0
+x = float(input("Digite o número: ")) # Entrada de número para que possa entrar na estrutura de repetição 
 
-valor = 1
-
-while (valor != 0):
-    valor = int(input("DIgite os números: "))
-    soma = soma + valor 
-print("A soma dos valores digitados: ", soma)
+soma = 0 # Essa variavel que irá calcular a soma. Ela deve começar com o valor 0 pois é um número neutro e que vai ser somada com a variavel X
+while (x != 0): # A estrutura vai se repetira enquanto a condição for verdadeira
+    soma = soma + x  # X vai se acumulando na variavel Soma 
+    x = float(input("Digite o número: ")) # O X irá receber um novo valor, que ira retorna para a condição while para verificar se a condição é falsa ou verdadeira 
+print (soma)
+  
