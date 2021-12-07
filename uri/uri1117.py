3c50131d0d0c0612d05ae649024a9b388e6a2b88
Lucas Farias De Carvalho Rocha <61882924+LucasRocha42@users.noreply.github.com>
2021-12-07 14:02:36 +0000

    While - score

---
+++ uri/uri1117.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+n1 = float(input("Nota 1: "))
+while (n1 < 0.0 and n1 > 10.0) :
+    print ("nota inválida")
+n1 = float(input("Nota 1: "))
+
+
+n2 = float(input("Nota 2: "))
+while (n2 < 0.0 and n2 > 10.0 ):
+  print("Nota válida")
+n2 = float(input("Nota 2: "))
+
+media = (n1 + n2) / 2.0
+
+print(media)
+
+n1 = float(input("Nota 1: "))
+n2 = float(input("Nota 2: "))
+
