21b77c62c9c48ebe62356d9533c3f865d1f54199
Lucas Farias De Carvalho Rocha <61882924+LucasRocha42@users.noreply.github.com>
2021-12-07 13:42:29 +0000

    Plano cartesiano - incompleto

---
+++ uri/uri1115.py
@@ -0,0 +1,18 @@
+x = int(input("Digite o X: "))
+y = int(input("Digite o Y: "))
+
+while (x != 0 and y != 0): 
+  if (x > 0 and y > 0):
+			print("primeiro");
+	    elif (x < 0 and y > 0)
+	  	print("segundo");
+			elif (x < 0 && y < 0) 
+			print("terceiro");
+			else 
+				print("quarto")
+			
+			
+
+
+
+
