787d93b718ec8935f8460b0cbfa8415f72554bf3
Lucas Farias De Carvalho Rocha <61882924+LucasRocha42@users.noreply.github.com>
2021-12-07 13:16:21 +0000

    Exercícios da Uri

---
+++ uri/uri1114.py
@@ -0,0 +1,9 @@
+senha = int(input("Digite a senha: "))
+senha_correta = 2002
+
+while (senha != senha_correta ):
+  print("Senha inválida")
+  senha = int(input("Digite a senha: "))
+print("Acesso permitido")
+
+  
