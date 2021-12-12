f0a35b4d82fb3566c8ac56952a2588a2ce9f1782
Lucas Farias De Carvalho Rocha <61882924+LucasRocha42@users.noreply.github.com>
2021-12-12 12:53:46 +0000

    Argumentos - nomeados e posicional

--- dev_aprende/funcoes.py
+++ dev_aprende/funcoes.py
@@ -11,7 +11,7 @@
 
 dar_boas_vindas_personalizado('Lucas')
  '''
-
+''' 
 # Desafios 1
 def gerar_nome_completo (nome, sobrenome): # Criando um função e passando os parametros (informações que vou receber)
   print(f'Boas-Vindas!{nome} {sobrenome}') # comando que vai ser executado 
@@ -24,3 +24,46 @@ def calcular_valores(preco, quantidade = 1):
   print (total)
 
 calcular_valores(12)
+ '''
+# Processar VS Retornar 
+# Funções que retornam e que processam 
+# Quando usar? Precisa dessa informação na lógica? ou só preciso processar esse dado?
+
+
+''' # Exemplos da aula
+# Argumentos posicionais - Valores devem está nas mesmas posições 
+def exibir_preco(nome_produto, preco):
+    print(f'{nome_produto} está no valor de: {preco}')
+exibir_preco('iphone', preco=5000  )
+
+# Argumentos nomeados - Valores são nomeados e passado no parametro 
+def exibir_preco(nome_produto, preco):
+    print(f'{nome_produto} está no valor de: {preco}')
+exibir_preco (nome_produto = 'iphone', preco = 5000)
+
+
+ '''
+def gerar_objeto_personalizado(cor,*,altura,formato): print(cor,altura,formato)
+
+gerar_objeto_personalizado ('azul',altura=1.7, formato='quadrado' )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
