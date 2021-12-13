0bef36987cdc4a77a6e8ea8664a75e9ba1ac033d
Lucas Farias De Carvalho Rocha <61882924+LucasRocha42@users.noreply.github.com>
2021-12-13 11:27:43 +0000

    listas

---
+++ dev_aprende/colecoes.py
@@ -0,0 +1,71 @@
+''' #Lista
+precos = [10,20,30,100]
+        #  0 1  2
+print(precos[2]) #acessando por indice
+
+#Encontrando os valores sem precisar do indice
+print(precos[precos.index(100)])
+
+# Lista aceita qualquer dado
+itens = [1,2,3,'ola',True,1.0]
+
+#Maneiras diferentes de gerar uma lista 
+#Multiplicação de valores(repetição)
+
+lista_de_noves = [9] * 10
+
+#Usando gerador range(sequencia)
+faixa = list(range(30))
+print(faixa)
+
+#Gerar a partir de String
+print(list('Olá'))
+
+#lista de lista (matriz)
+matriz_de_nome = [['Carol',30],['Marcus',50]]
+print(matriz_de_nome[0])
+print(matriz_de_nome[0][0])
+ '''
+
+#desafio 1
+''' itens = ['pc','livros','café' ]
+print(f'{itens}')
+
+#desafio 2
+lista = list(range(1, 132))
+print(lista)
+
+#desafio 3 
+print(lista, itens)
+
+#desafio 4
+matriz = [['pc',500],['livro', 30],['cafe', 10] ]
+print(matriz)
+ '''
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
