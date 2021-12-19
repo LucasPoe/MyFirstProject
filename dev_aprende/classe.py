35aad1041041b6b86a4e28c699faea14b0676e19
Lucas Farias De Carvalho Rocha <61882924+LucasRocha42@users.noreply.github.com>
2021-12-19 13:09:43 +0000

    Método construtor

--- dev_aprende/classe.py
+++ dev_aprende/classe.py
@@ -1,11 +1,11 @@
-# Classe = É um tipo estruturado que pode conter (membros)
+''' # Classe = É um tipo estruturado que pode conter (membros)
 # Atributo (dados/campos) membro
 # Métodos (funções/operações) membro
 
 # Definir uma classe 
 
 class Cubo: #cabeçalho da classe / Letra maiuscula PascalCase
-  ''' Docstring (recomendado)'''
+   Docstring (recomendado)
   # dado de classe compartilhado (Atributo da classe)
   def __init__ (self, valor): #Método constutor da classe 
     self.x = valor
@@ -18,3 +18,15 @@ class Cubo: #cabeçalho da classe / Letra maiuscula PascalCase
 teste = Cubo(520)
 c = teste.calcular_cubo()
 print(c)
+  '''
+# Método construtor 
+# Sintaxe - 
+# def __init__(self,[parâmetros]) 
+  # código do método construtor # Self - referencia ao objeto
+class Gato:
+  def __init__ (self, nome):
+    self.nome = nome
+    print('Um gato é um animal', self.nome )
+
+nome_gato = input('Digite o nome de seu gato: ')
+g1 = Gato(nome_gato)
