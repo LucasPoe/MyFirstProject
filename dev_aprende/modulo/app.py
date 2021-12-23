48480df3515d5dea487d086c313ef7c5199071e8
Lucas Farias De Carvalho Rocha <61882924+LucasRocha42@users.noreply.github.com>
2021-12-23 11:55:39 +0000

    Criando módulo

---
+++ dev_aprende/modulo/app.py
@@ -0,0 +1,4 @@
+from processamento import aumentar_volume,diminuir_volume
+
+aumentar_volume()
+diminuir_volume()
---
+++ dev_aprende/modulo/processamento.py
@@ -0,0 +1,4 @@
+def aumentar_volume():
+  print('Volume alto')
+def diminuir_volume():
+  print('Volume baixo')
